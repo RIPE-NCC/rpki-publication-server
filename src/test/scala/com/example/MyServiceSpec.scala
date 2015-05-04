e4af818b601a5d936568c9d6f8113559d13d798c
Misja Alma <malma@xebia.com>
2015-05-04 10:47:19 +0200

    Added (failing) tests for a publish and a withdraw request

--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -4,15 +4,30 @@ import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
+import scala.io.Source
 
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
+
+  def getFile(fileName: String) = Source.fromURL(getClass.getResource(fileName))
   
   "MyService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Post() ~> myRoute ~> check {
-        responseAs[String] must contain("Say hello")
+    "return an ok response for a valid publish request" in {
+      val publishXml = getFile("/publish.xml")
+      val publishXmlResponse = getFile("/publishResponse.xml")
+
+      Post("/", publishXml.mkString) ~> myRoute ~> check {
+        responseAs[String] must be(publishXmlResponse.mkString)
+      }
+    }
+
+    "return an ok response for a valid withdraw request" in {
+      val withdrawXml = getFile("/withdraw.xml")
+      val withdrawXmlResponse = getFile("/withdrawResponse.xml")
+
+      Post("/", withdrawXml.mkString) ~> myRoute ~> check {
+        responseAs[String] must be(withdrawXmlResponse.mkString)
       }
     }
 
