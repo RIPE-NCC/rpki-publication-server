ffe7a556eaaa1a2b07a583c51b918e13ab7694ab
Misja Alma <malma@xebia.com>
2015-05-04 16:59:38 +0200

    Proper service name

--- src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
@@ -13,7 +13,7 @@ object Boot extends App {
   implicit val system = ActorSystem("on-spray-can")
 
   // create and start our service actor
-  val service = system.actorOf(Props[PublicationServiceActor], "demo-service")
+  val service = system.actorOf(Props[PublicationServiceActor], "publication-service")
 
   implicit val timeout = Timeout(5.seconds)
   // start a new HTTP server on port 8080 with our service actor as the handler
