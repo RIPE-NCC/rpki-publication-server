a6cf8fbabf43e1c1e03b6bb72d5830341aebc669
Mikhail Puzanov <mpuzanov@ripe.net>
2015-05-04 10:52:09 +0200

    Add some initial code for XML parsing.

--- src/main/scala/com/example/Messages.scala
+++ src/main/scala/com/example/Messages.scala
@@ -1,16 +1,20 @@
 package com.example
 
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.xml.pull._
+
 case class Base64(s: String)
 
-class MsgType extends Enumeration {
+object MsgType extends Enumeration {
   type MsgType = Value
   val query = Value("query")
   val reply = Value("reply")
 }
 
-case class Msg(msgType: MsgType, pdus: Seq[Pdu])
+case class Msg(msgType: MsgType.MsgType, pdus: Seq[Pdu])
 
-case class Pdu()
+class Pdu()
 
 case class Publish(uri: String, base64: Base64) extends Pdu
 
@@ -18,5 +22,40 @@ case class Withdraw(uri: String) extends Pdu
 
 case class ReportError(code: String, message: Option[String]) extends Pdu
 
+object MsgXml {
 
+  def parse(xmlSring: String): Msg = {
+    // TODO Replace it with a stream
+    val xml = new XMLEventReader(Source.fromString(xmlSring))
+
+    def parse(xml: XMLEventReader) {
+      @tailrec
+      def loop(currNode: List[String]) {
+        if (xml.hasNext) {
+          xml.next match {
+            case EvElemStart(_, label, _, _) =>
+              println("Start element: " + label)
+              loop(label :: currNode)
+            case EvElemEnd(_, label) =>
+              println("End element: " + label)
+              loop(currNode.tail)
+            case EvText(text) =>
+              loop(currNode)
+            case _ => loop(currNode)
+          }
+        }
+      }
+      loop(List.empty)
+    }
+
+    parse(xml)
+
+    Msg(MsgType.query, Seq())
+  }
+
+  def serialize(msg: Msg): String = {
+    ""
+  }
+
+}
 
