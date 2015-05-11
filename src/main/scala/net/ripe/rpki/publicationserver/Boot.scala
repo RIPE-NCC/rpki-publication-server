61533fbdb0b6d031b486659d63a00aa0fadaa18d
Mikhail Puzanov <mpuzanov@ripe.net>
2015-05-11 12:18:08 +0200

    Change default port to 8090

--- src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
@@ -15,5 +15,5 @@ object Boot extends App {
 
   implicit val timeout = Timeout(5.seconds)
 
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8090)
 }
