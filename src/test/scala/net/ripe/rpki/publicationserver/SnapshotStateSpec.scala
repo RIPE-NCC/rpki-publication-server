42d859ccf3ff24ceb27a8c9a959375e961641a96
Mikhail Puzanov <mpuzanov@ripe.net>
2015-05-21 16:34:38 +0200

    Compilation fix.

--- src/test/scala/net/ripe/rpki/publicationserver/SnapshotStateSpec.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/SnapshotStateSpec.scala
@@ -4,7 +4,7 @@ class SnapshotStateSpec extends PublicationServerBaseSpec {
 
   test("should serialize a SnapshotState to proper xml") {
     val pdus: Map[String, (Base64, Hash)] = Map("rsync://bla" -> (Base64.apply("321"), Hash("123")))
-    val state = SnapshotState(SessionId("s123"), BigInt(123), pdus)
+    val state = new SnapshotState(SessionId("s123"), BigInt(123), pdus)
 
     val xml = SnapshotState.serialize(state).mkString
 
@@ -59,6 +59,5 @@ class SnapshotStateSpec extends PublicationServerBaseSpec {
   test("should fail to withdraw an object if hashes do not match") {
 
   }
-
->>>>>>> Add some tests for snapshot updates.
+  
 }
