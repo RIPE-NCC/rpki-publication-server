ecf12ec1537579f8f1bf9677d5df9a52057afcdf
Misja Alma <malma@xebia.com>
2015-05-21 09:16:48 +0200

    Updated schema to draft version 6, added a spec for withdraw request validation

--- src/test/scala/net/ripe/rpki/publicationserver/StaxParserSpec.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/StaxParserSpec.scala
@@ -8,33 +8,45 @@ class StaxParserSpec extends PublicationServerBaseSpec {
 
   before {
     // .rnc can't be handled by Woodstox or Stax. And the only schema that the .rnc could be converted to without loss of information, is .rng ...
-     schema = getFile("/schema.rng").mkString
+    schema = getFile("/schema.rng").mkString
   }
 
-  test("should parse and validate my xml file") {
-      val publishXml = getFile("/publish.xml")
+  test("should parse and validate my publish xml file") {
+    val publishXml = getFile("/publish.xml")
 
-      val parser = StaxParser.createFor(publishXml.mkString, schema)
+    val parser = StaxParser.createFor(publishXml.mkString, schema)
 
-      parser should not be null
+    parser should not be null
 
+    while (parser.hasNext) parser.next
+
+    parser.hasNext should be(false)
+  }
+
+  test("should parse and validate my withdraw xml file") {
+    val withdrawXml = getFile("/withdraw.xml")
+
+    val parser = StaxParser.createFor(withdrawXml.mkString, schema)
+
+    parser should not be null
+
+    while (parser.hasNext) parser.next
+
+    parser.hasNext should be(false)
+  }
+
+  test("should raise an exception when the request is invalid") {
+    val invalidXml = getFile("/invalidRequest.xml")
+
+    val parser = StaxParser.createFor(invalidXml.mkString, schema)
+
+    parser should not be null
+
+    val thrown = intercept[WstxValidationException] {
       while (parser.hasNext) parser.next
-
-      parser.hasNext should be(false)
     }
 
-    test("should raise an exception when the request is invalid") {
-      val invalidXml = getFile("/invalidRequest.xml")
-
-      val parser = StaxParser.createFor(invalidXml.mkString, schema)
-
-      parser should not be null
-
-      val thrown = intercept[WstxValidationException] {
-        while (parser.hasNext) parser.next
-      }
-
-      thrown.getMessage should include ("tag name \"foo\" is not allowed")
-    }
+    thrown.getMessage should include("tag name \"foo\" is not allowed")
+  }
 
 }
