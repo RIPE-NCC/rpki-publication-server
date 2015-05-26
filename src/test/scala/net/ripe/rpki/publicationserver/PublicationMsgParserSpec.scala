594d2306982204540c011fbf1bbd954e72b5ce49
Misja Alma <malma@xebia.com>
2015-05-26 10:51:41 +0200

    Fixed some failing tests

--- src/test/scala/net/ripe/rpki/publicationserver/PublicationMsgParserSpec.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/PublicationMsgParserSpec.scala
@@ -13,35 +13,35 @@ class PublicationMsgParserSpec extends PublicationServerBaseSpec {
 
   test("should parse publish message") {
     val publishXml = getFile("/publish.xml")
-    val msg = msgParser.process(publishXml.mkString).asInstanceOf[ReplyMsg]
+    val msg = msgParser.process(publishXml.mkString).right.get
 
-    val publishR = msg.pdus.head.asInstanceOf[PublishR]
-    publishR.uri should equal(new URI("rsync://wombat.example/Alice/blCrcCp9ltyPDNzYKPfxc.cer"))
-    publishR.tag should be(None)
+    val publishQ = msg.pdus.head.asInstanceOf[PublishQ]
+    publishQ.uri should equal(new URI("rsync://wombat.example/Alice/blCrcCp9ltyPDNzYKPfxc.cer"))
+    publishQ.tag should be(None)
   }
 
   test("should parse publish message with tag") {
     val publishXml = getFile("/publishWithTag.xml")
-    val msg = msgParser.process(publishXml.mkString).asInstanceOf[ReplyMsg]
+    val msg = msgParser.process(publishXml.mkString).right.get
 
-    val publishR = msg.pdus.head.asInstanceOf[PublishR]
-    publishR.tag should equal(Some("123"))
+    val publishQ = msg.pdus.head.asInstanceOf[PublishQ]
+    publishQ.tag should equal(Some("123"))
   }
 
   test("should parse withdraw message") {
     val withdrawXml = getFile("/withdraw.xml")
-    val msg = msgParser.process(withdrawXml.mkString).asInstanceOf[ReplyMsg]
+    val msg = msgParser.process(withdrawXml.mkString).right.get
 
-    val withdrawR = msg.pdus.head.asInstanceOf[WithdrawR]
-    withdrawR.uri should equal(new URI("rsync://wombat.example/Alice/blCrcCp9ltyPDNzYKPfxc.cer"))
-    withdrawR.tag should be(None)
+    val withdrawQ = msg.pdus.head.asInstanceOf[WithdrawQ]
+    withdrawQ.uri should equal(new URI("rsync://wombat.example/Alice/blCrcCp9ltyPDNzYKPfxc.cer"))
+    withdrawQ.tag should be(None)
   }
 
   test("should parse withdraw message with tag") {
     val withdrawXml = getFile("/withdrawWithTag.xml")
-    val msg = msgParser.process(withdrawXml.mkString).asInstanceOf[ReplyMsg]
+    val msg = msgParser.process(withdrawXml.mkString).right.get
 
-    val withdrawR = msg.pdus.head.asInstanceOf[WithdrawR]
-    withdrawR.tag should equal(Some("123"))
+    val withdrawQ = msg.pdus.head.asInstanceOf[WithdrawQ]
+    withdrawQ.tag should equal(Some("123"))
   }
 }
