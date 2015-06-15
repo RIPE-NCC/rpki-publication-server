4cd7b52adfde29de6f89fdd36a1f5517587dccab
Oleg Muravskiy <oleg@ripe.net>
2015-06-15 09:35:48 +0200

    Refined Cache-Control: header

--- src/main/scala/net/ripe/rpki/publicationserver/PublicationService.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/PublicationService.scala
@@ -134,19 +134,26 @@ trait PublicationService extends HttpService with RepositoryPath {
 
 
 trait RRDPService extends HttpService with RepositoryPath {
+  val immutableContentValiditySeconds: Long = 31536000 // ~one year
 
   val rrdpRoutes =
     path("notification.xml") {
-      respondWithHeader(`Cache-Control`(CacheDirectives.`no-cache`, CacheDirectives.`no-store`, CacheDirectives.`must-revalidate`)) {
-        serve( s"""$repositoryPath/notification.xml""")
+      respondWithHeader(`Cache-Control`(CacheDirectives.`no-cache`)) {
+        serve(s"$repositoryPath/notification.xml")
       }
     } ~
     path(JavaUUID / IntNumber / "snapshot.xml") { (sessionId, serial) =>
-      serve( s"""$repositoryPath/$sessionId/$serial/snapshot.xml""")
+      serveImmutableContent(s"$repositoryPath/$sessionId/$serial/snapshot.xml")
     } ~
     path(JavaUUID / IntNumber / "delta.xml") { (sessionId, serial) =>
-      serve( s"""$repositoryPath/$sessionId/$serial/delta.xml""")
+      serveImmutableContent(s"$repositoryPath/$sessionId/$serial/delta.xml")
     }
+
+  private def serveImmutableContent(filename: => String) = {
+    respondWithHeader(`Cache-Control`(CacheDirectives.`max-age`(immutableContentValiditySeconds))) {
+      serve(filename)
+    }
+  }
 
   private def serve(filename: => String) = get {
     respondWithMediaType(`application/xhtml+xml`) {
