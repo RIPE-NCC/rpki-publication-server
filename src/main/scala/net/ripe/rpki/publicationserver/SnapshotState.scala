687a14d5365aadadaf4ab3e7cc0cd54c5d3e8551
Mikhail Puzanov <mpuzanov@ripe.net>
2015-06-23 11:41:33 +0200

    Fix updateWith implementation.

--- src/main/scala/net/ripe/rpki/publicationserver/SnapshotState.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/SnapshotState.scala
@@ -4,10 +4,16 @@ import java.util.UUID
 
 import com.softwaremill.macwire.MacwireMacros._
 import net.ripe.rpki.publicationserver.model._
-import net.ripe.rpki.publicationserver.store.{DeltaStore, ServerStateStore, ObjectStore, DB}
 import net.ripe.rpki.publicationserver.store.fs.RepositoryWriter
+import net.ripe.rpki.publicationserver.store.{DB, DeltaStore, ObjectStore, ServerStateStore}
+import slick.dbio.DBIO
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
+import slick.driver.H2Driver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 
 /**
@@ -54,18 +60,47 @@ trait SnapshotStateService extends Urls with Logging with Hashing {
   def updateWith(clientId: ClientId, queries: Seq[QueryPdu]): Seq[ReplyPdu] = synchronized {
     val oldServerState = serverStateStore.get
     val newServerState = oldServerState.next
-    persistForClient(clientId, queries, newServerState) { replies: Seq[ReplyPdu] =>
-      serverStateStore.update(newServerState)
-      val deltas = deltaStore.getDeltas
-      val snapshot = Snapshot(newServerState, objectStore.listAll)
-      val newNotification = Notification.create(snapshot, newServerState, deltas)
-      repositoryWriter.writeNewState(conf.locationRepositoryPath, newServerState, deltas, newNotification, snapshot) match {
-        case Success(_) =>
-          notificationState.update(newNotification)
-          Seq()
-        case Failure(e) =>
-          logger.error("Could not write XML files to filesystem: " + e.getMessage, e)
-          Seq(ReportError(BaseError.CouldNotPersist, Some("Could not write XML files to filesystem: " + e.getMessage)))
+    val results = getPersistAction(clientId, queries, newServerState)
+
+    val errors = results.collect { case Left(error) => error }
+    if (errors.nonEmpty) {
+      errors
+    }
+    else {
+      val replies = results.collect { case Right((reply, _, _)) => reply }
+      val actions = results.collect { case Right((_, action, _)) => action }
+      val validPdus = results.collect { case Right((_, _, qPdu)) => qPdu }
+
+      try {
+        val deltaAction = deltaStore.addDeltaAction(clientId, Delta(sessionId, newServerState.serialNumber, validPdus))
+
+        val action = (for {
+          _           <- DBIO.seq(actions: _*)
+          _           <- DBIO.seq(deltaAction)
+          _           <- serverStateStore.updateAction(newServerState)
+          objActions  <- objectStore.getAllAction
+          x <- {
+            val deltas = deltaStore.getDeltas
+            val snapshot = Snapshot(newServerState, objActions)
+            val newNotification = Notification.create(snapshot, newServerState, deltas)
+            repositoryWriter.writeNewState(conf.locationRepositoryPath, newServerState, deltas, newNotification, snapshot) match {
+              case Success(_) =>
+                notificationState.update(newNotification)
+                DBIO.successful(replies)
+              case Failure(e) =>
+                logger.error("Could not write XML files to filesystem: " + e.getMessage, e)
+                DBIO.failed(e)
+            }
+          }
+        } yield x
+          ).transactionally
+
+        Await.result(db.run(action), Duration.Inf)
+        replies
+      } catch {
+        case e: Exception =>
+          logger.error("Couldn't persist objects", e)
+          Seq(ReportError(BaseError.CouldNotPersist, Some(s"A problem occurred while persisting the changes: " + e)))
       }
     }
   }
@@ -73,8 +108,8 @@ trait SnapshotStateService extends Urls with Logging with Hashing {
   /*
    * TODO Check if the client doesn't try to modify objects that belong to other client
    */
-  def persistForClient(clientId: ClientId, queries: Seq[QueryPdu], serverState: ServerState)(f : Seq[ReplyPdu] => Seq[ReplyPdu]) = {
-    val result = queries.map {
+  def getPersistAction(clientId: ClientId, queries: Seq[QueryPdu], serverState: ServerState) = {
+    queries.map {
       case pq@PublishQ(uri, tag, None, base64) =>
         objectStore.find(uri) match {
           case Some((_, h, _)) =>
@@ -114,27 +149,6 @@ trait SnapshotStateService extends Urls with Logging with Hashing {
           case None =>
             Left(ReportError(BaseError.NoObjectForWithdraw, Some(s"No object [$uri] found.")))
         }
-    }
-
-    val errors = result.collect { case Left(error) => error }
-    if (errors.nonEmpty) {
-      errors
-    }
-    else {
-      val replies = result.collect {
-        case Right((reply, _, _)) => reply
-      }
-      val actions = result.collect { case Right((_, action, _)) => action }
-      val validPdus = result.collect { case Right((_, _, qPdu)) => qPdu }
-      try {
-        deltaStore.addDelta(clientId, Delta(sessionId, serverState.serialNumber, validPdus))
-        val transactionReplies = objectStore.atomic(actions, f(replies))
-        replies ++ transactionReplies
-      } catch {
-        case e: Exception =>
-          logger.error("Couldn't persist objects", e)
-          Seq(ReportError(BaseError.CouldNotPersist, Some(s"A problem occurred while persisting the changes: " + e)))
-      }
     }
   }
 
