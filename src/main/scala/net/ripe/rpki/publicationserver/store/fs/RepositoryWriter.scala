2c76d2bb0e51e84d189c0aeb9ad3496a6c85f83f
Oleg Muravskiy <oleg@ripe.net>
2015-06-24 18:36:15 +0200

    Use only NIO.2 file operations.

--- src/main/scala/net/ripe/rpki/publicationserver/store/fs/RepositoryWriter.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/store/fs/RepositoryWriter.scala
@@ -1,8 +1,6 @@
 package net.ripe.rpki.publicationserver.store.fs
 
-import java.io.{File, FileWriter}
 import java.nio.file._
-import com.softwaremill.macwire.MacwireMacros._
 
 import net.ripe.rpki.publicationserver._
 import net.ripe.rpki.publicationserver.model.{Snapshot, ServerState, Notification, Delta}
@@ -44,63 +42,49 @@ class RepositoryWriter extends Logging {
   def writeSnapshot(rootDir: String, serverState: ServerState, snapshot: Snapshot) = {
     val ServerState(sessionId, serial) = serverState
     val stateDir = getStateDir(rootDir, sessionId.toString, serial)
-    writeFile(snapshot.serialized, new File(stateDir, "snapshot.xml"))
+    writeFile(snapshot.serialized, stateDir.resolve("snapshot.xml"))
   }
 
   def writeDelta(rootDir: String, delta: Delta) = {
     val stateDir = getStateDir(rootDir, delta.sessionId.toString, delta.serial)
-    writeFile(delta.serialize.mkString, new File(stateDir, "delta.xml"))
+    writeFile(delta.serialize.mkString, stateDir.resolve("delta.xml"))
   }
 
   def writeNotification(rootDir: String, notification: Notification) = {
     val root = getRootFolder(rootDir)
 
-    val tmpFile = new File(root, "notification_tmp.xml")
-    writeFile(notification.serialized, tmpFile)
-
-    val source = Paths.get(tmpFile.toURI)
-    val target = Paths.get(new File(root, "notification.xml").toURI)
-    Files.move(source, target, StandardCopyOption.REPLACE_EXISTING)
+    val tmpFile = Files.createTempFile(root, "notification.", ".xml")
+    try {
+      writeFile(notification.serialized, tmpFile)
+      val target = root.resolve("notification.xml")
+      Files.move(tmpFile, target, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE)
+    } finally {
+      Files.deleteIfExists(tmpFile)
+    }
   }
 
-  private def writeFile(content: String, file: File) = {
-    val writer = new FileWriter(file)
-    try writer.write(content)
-    finally writer.close()
+  private def writeFile(content: String, path: Path) = {
+    Files.write(path, content.getBytes("UTF-8"))
   }
 
-  private def getRootFolder(rootDir: String): File = {
-    val root = new File(rootDir)
-    if (!root.exists()) root.mkdir()
-    root
+  private def getRootFolder(rootDir: String): Path = {
+    Files.createDirectories(Paths.get(rootDir))
   }
 
-  private def getStateDir(rootDir: String, sessionId: String, serial: Long): File = {
-    val root = getRootFolder(rootDir)
-    dir(dir(root, sessionId), serial.toString)
-  }
-
-  private def dir(d: File, name: String) = {
-    val _dir = new File(d, name)
-    if (!_dir.exists()) _dir.mkdir()
-    _dir
+  private def getStateDir(rootDir: String, sessionId: String, serial: Long): Path = {
+    Files.createDirectories(Paths.get(rootDir, sessionId, String.valueOf(serial)))
   }
 
   def deleteSessionFile(rootDir: String, serverState: ServerState, name: String) = {
     val ServerState(sessionId, serial) = serverState
-    val sessionDir = new File(new File(rootDir), sessionId.toString)
-    val serialDir = new File(sessionDir, serial.toString)
-    del(new File(serialDir, "snapshot.xml"))
+    Files.deleteIfExists(Paths.get(rootDir, sessionId.toString, serial.toString, "snapshot.xml"))
   }
-
-  def del(f: File) = Files.deleteIfExists(Paths.get(f.toURI))
 
   def deleteSnapshot(rootDir: String, serverState: ServerState) = deleteSessionFile(rootDir, serverState, "snapshot.xml")
 
   def deleteDelta(rootDir: String, serverState: ServerState) = deleteSessionFile(rootDir, serverState, "delta.xml")
 
   def deleteNotification(rootDir: String) = {
-    del(new File(rootDir, "notification.xml"))
-    del(new File(rootDir, "notification_tmp.xml"))
+    Files.deleteIfExists(Paths.get(rootDir, "notification.xml"))
   }
 }
