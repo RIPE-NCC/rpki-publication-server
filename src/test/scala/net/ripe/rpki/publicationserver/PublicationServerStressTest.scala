dfdf5c8c56ee2f73392e7eb428db7edfeeee5f4d
Misja Alma <malma@xebia.com>
2015-06-25 16:11:10 +0200

    larger timeout on requests in stresstest because it fails on bamboo

--- src/test/scala/net/ripe/rpki/publicationserver/PublicationServerStressTest.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/PublicationServerStressTest.scala
@@ -54,7 +54,7 @@ class PublicationServerStressTest extends PublicationServerBaseTest with Scalate
 
   test("should get correct results for 100 sequential client requests") {
     val futures = getPublishRetrieveFutures(100)
-    futures.foreach(f => Await.ready(f, Duration.fromNanos(oneSecond * 1)))
+    futures.foreach(f => Await.ready(f, Duration.fromNanos(oneSecond * 3)))
   }
 
   test("should get correct results for 100 parallel client requests") {
