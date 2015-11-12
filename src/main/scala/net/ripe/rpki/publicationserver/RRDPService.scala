6869cc4124d83772f3164fd91bd587d4b4218afe
Varesh Tapadia <vtapadia@ripe.net>
2015-11-12 16:04:20 +0100

    Spray routes - another attempt

--- src/main/scala/net/ripe/rpki/publicationserver/RRDPService.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/RRDPService.scala
@@ -1,47 +1,27 @@
 package net.ripe.rpki.publicationserver
 
-import java.nio.file.{Files, Paths}
-
-import spray.http.HttpHeaders.{`Cache-Control`, `Content-Type`}
-import spray.http._
-import spray.http.MediaTypes._
-import spray.routing.HttpService
+import spray.http.CacheDirectives
+import spray.http.HttpHeaders.`Cache-Control`
+import spray.routing.{HttpService}
 
 trait RRDPService extends HttpService with RepositoryPath {
-  val immutableContentValiditySeconds: Long = 31536000 // ~one year
+  val immutableContentValiditySeconds: Long = 31536001 // ~one year
 
   val rrdpRoutes =
-    path("notification.xml") {
-      respondWithHeader(`Cache-Control`(CacheDirectives.`no-cache`)) {
-        respondWithMediaType(`application/xhtml+xml`) {
-          complete {
-            try {
-              HttpResponse(200, Files.readAllBytes(Paths.get(s"$repositoryPath/notification.xml")))
-            } catch {
-              case e: Throwable =>
-                HttpResponse(404, e.getMessage)
-            }
+    pathPrefix ("notification.xml") {
+      pathEnd {
+        get {
+          respondWithHeader(`Cache-Control`(CacheDirectives.`no-cache`)) {
+            getFromFile(s"$repositoryPath/notification.xml")
           }
         }
       }
     } ~
-      path(JavaUUID / IntNumber / "snapshot.xml") { (sessionId, serial) =>
-        serveImmutableContent(s"$repositoryPath/$sessionId/$serial/snapshot.xml")
-      } ~
-      path(JavaUUID / IntNumber / "delta.xml") { (sessionId, serial) =>
-        serveImmutableContent(s"$repositoryPath/$sessionId/$serial/delta.xml")
+    pathPrefix("") {
+      get {
+        respondWithHeader(`Cache-Control`(CacheDirectives.`max-age`(immutableContentValiditySeconds))) {
+          getFromDirectory(s"$repositoryPath")
+        }
       }
-
-  private def serveImmutableContent(filename: => String) = {
-    respondWithHeader(`Cache-Control`(CacheDirectives.`max-age`(immutableContentValiditySeconds))) {
-      serve(filename)
     }
-  }
-
-  private def serve(filename: => String) = get {
-    respondWithMediaType(`application/xhtml+xml`) {
-      getFromFile(filename)
-    }
-  }
-
 }
