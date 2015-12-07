4394c5f4e2c96545d0c86b926fc0e7500d57637b
Oleg Muravskiy <oleg@ripe.net>
2015-12-07 16:31:35 +0100

    Minimal implementation of Rsync repository writer

--- src/main/scala/net/ripe/rpki/publicationserver/AppConfig.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/AppConfig.scala
@@ -1,6 +1,7 @@
 package net.ripe.rpki.publicationserver
 
 import java.net.URI
+import java.nio.file.{Paths, Path}
 import java.util.Map.Entry
 import java.util.concurrent.TimeUnit
 
@@ -29,14 +30,14 @@ class AppConfig extends ConfigWrapper {
   lazy val locationLogfile = getConfig.getString("locations.logfile")
   lazy val unpublishedFileRetainPeriod = Duration(getConfig.getDuration("unpublished-file-retain-period", TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS)
   lazy val defaultTimeout = Duration(getConfig.getDuration("default.timeout", TimeUnit.MINUTES), TimeUnit.MINUTES)
-  lazy val rsyncRepositoryMapping : Map[URI, String] = {
+  lazy val rsyncRepositoryMapping : Map[URI, Path] = {
     val list : Iterable[ConfigObject] = getConfig.getObjectList("locations.rsync.repository-mapping").asScala
     (for {
       item : ConfigObject <- list
       entry : Entry[String, ConfigValue] <- item.entrySet().asScala
       uri = URI.create(entry.getKey)
-      dir = entry.getValue.unwrapped().toString
-    } yield (uri, dir)).toMap
+      path = Paths.get(entry.getValue.unwrapped().toString)
+    } yield (uri, path)).toMap
   }
 }
 
--- src/main/scala/net/ripe/rpki/publicationserver/store/fs/RsyncRepositoryWriter.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/store/fs/RsyncRepositoryWriter.scala
@@ -1,10 +1,46 @@
 package net.ripe.rpki.publicationserver.store.fs
 
-import net.ripe.rpki.publicationserver.Logging
+import java.io.ByteArrayInputStream
+import java.net.URI
+import java.nio.file.attribute.PosixFilePermissions
+import java.nio.file.{Files, Path, StandardCopyOption}
+
+import com.softwaremill.macwire.MacwireMacros._
 import net.ripe.rpki.publicationserver.model.Delta
+import net.ripe.rpki.publicationserver.{AppConfig, Base64, Logging, PublishQ, WithdrawQ}
+
+import scala.util.Try
 
 class RsyncRepositoryWriter extends Logging {
-  def writeDelta(rootDir: String, delta: Delta): Unit = {
+  lazy val conf = wire[AppConfig]
 
+  val newDirectoryPermissions = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwxrwxr-x"))
+
+  def resolvePath(uri: URI): Path = {
+    conf.rsyncRepositoryMapping.collectFirst {
+      case (rootUri,rootPath) if rootUri.relativize(uri) != uri =>
+        rootPath.resolve(rootUri.relativize(uri).toString)
+    }.get
+  }
+
+  def writeFile(uri: URI, base64: Base64): Unit = {
+    val inputStream = new ByteArrayInputStream(base64.value.getBytes("UTF-8"))
+    val target: Path = resolvePath(uri)
+    Files.createDirectories(target.getParent, newDirectoryPermissions)
+    Files.copy(java.util.Base64.getDecoder.wrap(inputStream), target, StandardCopyOption.REPLACE_EXISTING)
+  }
+
+  def removeFile(uri: URI): Unit = {
+    Files.deleteIfExists(resolvePath(uri))
+  }
+
+  def writeDelta(rootDir: String, delta: Delta) = Try {
+    delta.pdus.foreach {
+      case PublishQ(uri, tag, hash, base64) =>
+        writeFile(uri, base64)
+      case WithdrawQ(uri, tag, hash) =>
+        removeFile(uri)
+      case unknown => throw new UnsupportedOperationException(s"Unknown PDU in delta: $unknown")
+    }
   }
 }
