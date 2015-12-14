25faeee9746cd656e09b433b5a4d45c2d9445cdc
Mikhail Puzanov <mpuzanov@ripe.net>
2015-12-14 13:51:23 +0100

    Add throttling to the snapshot writing

--- src/main/scala/net/ripe/rpki/publicationserver/store/fs/FSWriterActor.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/store/fs/FSWriterActor.scala
@@ -5,8 +5,8 @@ import java.nio.file.attribute.FileTime
 import akka.actor._
 import com.softwaremill.macwire.MacwireMacros._
 import net.ripe.rpki.publicationserver.model.{Delta, Notification, ServerState, Snapshot}
-import net.ripe.rpki.publicationserver.store.{DeltaStore, ObjectStore}
-import net.ripe.rpki.publicationserver.{Config, Logging}
+import net.ripe.rpki.publicationserver.store.{ServerStateStore, DeltaStore, ObjectStore}
+import net.ripe.rpki.publicationserver.{PublicationServiceActor, Config, Logging}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
@@ -16,6 +16,31 @@ import scala.util.{Try, Failure, Success}
 case class InitCommand(newServerState: ServerState)
 case class WriteCommand(newServerState: ServerState)
 case class CleanSnapshotsCommand(timestamp: FileTime, latestSerial: Long)
+
+case class UpdateSnapsot()
+case class SetTarget(actor: ActorRef)
+
+class Throttler extends Actor with Logging with Config {
+
+  import context._
+
+  var target: ActorRef = _
+
+  var scheduled = false
+
+  override def receive = {
+    case SetTarget(t) =>
+      target = t
+      scheduled = false
+
+    case UpdateSnapsot =>
+      if (!scheduled) {
+        system.scheduler.scheduleOnce(10.seconds, target, UpdateSnapsot())
+        scheduled = true
+      }
+  }
+}
+
 
 class FSWriterActor extends Actor with Logging with Config {
   import context._
@@ -27,6 +52,15 @@ class FSWriterActor extends Actor with Logging with Config {
 
   protected val objectStore = ObjectStore.get
 
+  protected val serverStateStore = ServerStateStore.get
+
+  private var throttler: ActorRef = _
+
+  override def preStart() = {
+    throttler = system.actorOf(Props(new Throttler), "snapshot-writing-throttler")
+    throttler ! SetTarget(self)
+  }
+
   override def receive = {
     case InitCommand(newServerState) =>
       Try(initFSContent(newServerState)).recover { case e =>
@@ -34,14 +68,17 @@ class FSWriterActor extends Actor with Logging with Config {
       }.get
 
     case CleanSnapshotsCommand(timestamp, latestSerial) =>
-      Try(cleanupSnapshots(timestamp, latestSerial)).failed.foreach {
-        logger.error("Error processing command", _)
-      }
+      tryProcess(cleanupSnapshots(timestamp, latestSerial))
 
     case WriteCommand(newServerState) =>
-      Try(updateFSContent(newServerState)).failed.foreach {
-        logger.error("Error processing command", _)
-      }
+      tryProcess(updateFSContent(newServerState))
+
+    case UpdateSnapsot =>
+      tryProcess(updateFSSnapshot())
+  }
+
+  def tryProcess[T](f : => T) = Try(f).failed.foreach {
+    logger.error("Error processing command", _)
   }
 
   def initFSContent(newServerState: ServerState): Unit = {
@@ -90,42 +127,49 @@ class FSWriterActor extends Actor with Logging with Config {
   def updateFSContent(newServerState: ServerState): Unit = {
     val givenSerial = newServerState.serialNumber
 
+    updateFSDelta(givenSerial)
+
+    throttler ! UpdateSnapsot()
+  }
+
+  def updateFSDelta(givenSerial: Long): Unit = {
     deltaStore.getDelta(givenSerial) match {
       case None =>
         logger.error(s"Could not find delta $givenSerial")
       case Some(delta) =>
-        logger.info(s"Writing delta $givenSerial to filesystem")
-        rrdpWriter.writeDelta(conf.rrdpRepositoryPath, delta).recover {
-          case e: Exception =>
-            logger.error(s"Could not write delta $givenSerial to RRDP repo: ", e)
-        }
+        logger.debug(s"Writing delta $givenSerial to rsync filesystem")
         rsyncWriter.writeDelta(delta).recover {
           case e: Exception =>
             logger.error(s"Could not write delta $givenSerial to rsync repo: ", e)
         }
-    }
-
-    objectStore.listAll(givenSerial) match {
-      case None =>
-        logger.info(s"Skipping snapshot $givenSerial")
-
-      case Some(objects) =>
-        logger.info(s"Writing snapshot $givenSerial to filesystem")
-        val snapshot = Snapshot(newServerState, objects)
-
-        val deltas = deltaStore.markOldestDeltasForDeletion(snapshot.binarySize, conf.unpublishedFileRetainPeriod)
-
-        val (deltasToPublish, deltasToDelete) = deltas.partition(_.whenToDelete.isEmpty)
-
-        val newNotification = Notification.create(snapshot, newServerState, deltasToPublish.toSeq)
-        val now = System.currentTimeMillis
-        rrdpWriter.writeNewState(conf.rrdpRepositoryPath, newServerState, newNotification, snapshot) match {
-          case Success(timestampOption) =>
-            if (timestampOption.isDefined) scheduleSnapshotCleanup(timestampOption.get, givenSerial)
-            cleanupDeltas(deltasToDelete.filter(_.whenToDelete.exists(_.getTime < now)))
-          case Failure(e) =>
-            logger.error("Could not write XML files to filesystem: " + e.getMessage, e)
+        logger.debug(s"Writing delta $givenSerial to RRDP filesystem")
+        rrdpWriter.writeDelta(conf.rrdpRepositoryPath, delta).recover {
+          case e: Exception =>
+            logger.error(s"Could not write delta $givenSerial to RRDP repo: ", e)
         }
+    }
+  }
+
+
+  def updateFSSnapshot(): Unit = {
+    val serverState = serverStateStore.get
+    val objects = objectStore.listAll
+
+    logger.info(s"Writing snapshot ${serverState.serialNumber} to filesystem")
+    val snapshot = Snapshot(serverState, objects)
+
+    val deltas = deltaStore.markOldestDeltasForDeletion(snapshot.binarySize, conf.unpublishedFileRetainPeriod)
+
+    val (deltasToPublish, deltasToDelete) = deltas.partition(_.whenToDelete.isEmpty)
+
+    val newNotification = Notification.create(snapshot, serverState, deltasToPublish.toSeq)
+    val now = System.currentTimeMillis
+    rrdpWriter.writeNewState(conf.rrdpRepositoryPath, serverState, newNotification, snapshot) match {
+      case Success(timestampOption) =>
+        if (timestampOption.isDefined) scheduleSnapshotCleanup(timestampOption.get, serverState.serialNumber)
+        cleanupDeltas(deltasToDelete.filter(_.whenToDelete.exists(_.getTime < now)))
+      case Failure(e) =>
+        logger.error("Could not write XML files to filesystem: " + e.getMessage, e)
     }
   }
 
@@ -148,5 +192,5 @@ class FSWriterActor extends Actor with Logging with Config {
 }
 
 object FSWriterActor {
-  def props = Props(new FSWriterActor())
+  def props() = Props(new FSWriterActor())
 }
