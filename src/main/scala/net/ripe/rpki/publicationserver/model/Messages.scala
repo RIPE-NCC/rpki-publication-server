ac672a662e1e7e7c51de8085c205ad7cb8c56286
Mikhail Puzanov <mpuzanov@ripe.net>
2016-01-15 13:18:21 +0100

    Add initial message types.

---
+++ src/main/scala/net/ripe/rpki/publicationserver/model/Messages.scala
@@ -0,0 +1,17 @@
+package net.ripe.rpki.publicationserver.model
+
+import java.net.URI
+
+import net.ripe.rpki.publicationserver.{Base64, Hash, QueryMessage}
+
+object Messages {
+
+  type State = Map[URI, (Base64, Hash)]
+
+  case class RawMessage(queryMessage: QueryMessage)
+
+  case class ValidatedMessage(queryMessage: QueryMessage, state: State)
+
+  case class BatchMessage(messages: Seq[ValidatedMessage], state: State)
+}
+
