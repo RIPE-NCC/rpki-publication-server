ef01d65c97cbc578332910ea8e567039a835624a
Mikhail Puzanov <mpuzanov@ripe.net>
2016-02-23 14:05:10 +0100

    Return back the implicit, it breaks SSL (implicits are evil!)

--- src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/Boot.scala
@@ -33,7 +33,7 @@ object Boot extends App {
 
   implicit val timeout = Timeout(5.seconds)
 
-  val sslContext: SSLContext = {
+  implicit val sslContext: SSLContext = {
     val sslContext = SSLContext.getInstance("TLS")
     sslContext.init(getKeyManagers, getTrustManagers, null)
     sslContext
