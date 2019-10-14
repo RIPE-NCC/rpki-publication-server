8202809afd982ec22ad19587e4c5d04aa21e6d03
Mikhail Puzanov <misha.puzanov@gmail.com>
2019-10-14 12:30:37 +0200

    Optimize XML generation: don't use intermediate DOM representation, just write the bytes directly.

--- src/main/scala/net/ripe/rpki/publicationserver/model/Snapshot.scala
+++ src/main/scala/net/ripe/rpki/publicationserver/model/Snapshot.scala
@@ -1,35 +1,33 @@
 package net.ripe.rpki.publicationserver.model
 
+import java.io.ByteArrayOutputStream
 import java.net.URI
 
 import net.ripe.rpki.publicationserver.{Base64, Hashing}
 
-import scala.xml.{Elem, Node}
-
 case class Snapshot(serverState: ServerState, pdus: Seq[(Base64, URI)]) extends Hashing {
 
-  lazy val bytes = serialize.mkString.getBytes
+  lazy val bytes = serialize
   lazy val contentHash = hash(bytes)
   lazy val binarySize = bytes.length
 
-  private[model] def serialize = {
-    val ServerState(sessionId, serial) = serverState
-    snapshotXml(
-      sessionId.toString,
-      serial,
-      pdus.map { e =>
-        val (base64, uri) = e
-        <publish uri={uri.toString}>
-          {base64.value}
-        </publish>
-      }
-    )
+  def streamChars(s: String, stream: ByteArrayOutputStream): Unit = {
+    s.toCharArray.foreach(c => stream.write(c))
   }
 
-  private def snapshotXml(sessionId: String, serial: BigInt, pdus: => Iterable[Node]): Elem =
-    <snapshot xmlns="http://www.ripe.net/rpki/rrdp" version="1" session_id={sessionId} serial={serial.toString()}>
-      {pdus}
-    </snapshot>
+  private[model] def serialize = {
+    val ServerState(sessionId, serial) = serverState
+    val stream = new ByteArrayOutputStream()
+    streamChars("<snapshot version=\"1\" session_id=\"" + sessionId + "\" serial=\"" + serial + "\" xmlns=\"http://www.ripe.net/rpki/rrdp\">", stream)
+    pdus.foreach { pdu =>
+      val (base64, uri) = pdu
+      streamChars("<publish uri=\"" + uri + "\">", stream)
+      streamChars(base64.value, stream)
+      streamChars("</publish>", stream)
+    }
+    streamChars("</snapshot>", stream)
+    stream.toByteArray
+  }
 
 }
 
--- src/test/scala/net/ripe/rpki/publicationserver/model/SnapshotTest.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/model/SnapshotTest.scala
@@ -3,7 +3,7 @@ package net.ripe.rpki.publicationserver.model
 import java.net.URI
 import java.util.UUID
 
-import net.ripe.rpki.publicationserver.{Base64, Hash, PublicationServerBaseTest}
+import net.ripe.rpki.publicationserver.{Base64, PublicationServerBaseTest}
 
 class SnapshotTest extends PublicationServerBaseTest {
 
@@ -11,10 +11,13 @@ class SnapshotTest extends PublicationServerBaseTest {
     val sessionId = UUID.randomUUID()
     val state = Snapshot(ServerState(sessionId, 123L), Seq((Base64("321"), new URI("rsync://bla"))))
 
-    val xml = state.serialize.mkString
+    val xml = state.serialize
 
-    trim(xml) should be(trim(s"""<snapshot version="1" session_id="$sessionId" serial="123" xmlns="http://www.ripe.net/rpki/rrdp">
-                                  <publish uri="rsync://bla">321</publish>
-                                </snapshot>"""))
+    xml should be(toBytes(
+      s"""<snapshot version="1" session_id="$sessionId" serial="123" xmlns="http://www.ripe.net/rpki/rrdp"><publish uri="rsync://bla">321</publish></snapshot>"""))
+  }
+
+  private def toBytes(s: String) = {
+    s.map(c => c.toByte).toArray
   }
 }
