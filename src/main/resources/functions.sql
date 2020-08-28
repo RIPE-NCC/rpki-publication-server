1e039dca7cae7cbf6ab2eefa5cdd5876ee5ad3bc
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-08-28 12:57:39 +0200

    Implement some PG functions to manipulate data, adjust tables.

---
+++ src/main/resources/functions.sql
@@ -0,0 +1,136 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION last_pending_version() RETURNS BIGINT AS
+$body$
+DECLARE
+    the_version    BIGINT;
+    the_serial     BIGINT;
+    the_session_id TEXT;
+BEGIN
+    PERFORM pg_advisory_lock(9999);
+
+    SELECT version
+    INTO the_version
+    FROM versions
+    WHERE state = 'pending';
+
+    IF FOUND THEN
+        RETURN the_version;
+    ELSE
+        SELECT version + 1, session_id, serial
+        INTO the_version, the_session_id, the_serial
+        FROM versions
+        ORDER BY version DESC
+        LIMIT 1;
+
+        INSERT INTO versions(version, session_id, state, serial)
+        VALUES (the_version, the_session_id, 'pending', the_serial + 1);
+    END IF;
+
+    PERFORM pg_advisory_unlock(9999);
+
+    RETURN the_version;
+END
+$body$
+    LANGUAGE plpgsql;
+
+
+-- TODO Generate session id internally
+CREATE OR REPLACE FUNCTION init_versions(session_id_ TEXT) RETURNS BIGINT AS
+$body$
+BEGIN
+    PERFORM pg_advisory_lock(9999);
+
+    IF (SELECT COUNT(*) FROM versions) = 0 THEN
+        INSERT INTO versions(version, session_id, state, serial)
+        VALUES (1, session_id_, 'pending', 1);
+    END IF;
+
+    PERFORM pg_advisory_unlock(9999);
+
+    RETURN 1;
+END
+$body$
+    LANGUAGE plpgsql;
+
+
+-- TODO Do something with object_log entries as well
+CREATE OR REPLACE FUNCTION mark_pending_as_generated() RETURNS VOID AS
+$body$
+BEGIN
+    PERFORM pg_advisory_lock(9999);
+
+    WITH generated_version AS (
+        UPDATE versions SET state = 'generated'
+            WHERE state = 'pending'
+            RETURNING version
+    )
+    DELETE
+    FROM object_log
+    WHERE version IN (SELECT version FROM generated_version);
+
+    PERFORM pg_advisory_unlock(9999);
+END
+$body$
+    LANGUAGE plpgsql;
+
+
+-- Add object, corresponds to
+CREATE OR REPLACE FUNCTION create_object(version_ BIGINT,
+                                         bytes_ BYTEA,
+                                         url_ TEXT,
+                                         client_id_ TEXT) RETURNS VOID AS
+$body$
+DECLARE
+    existing_object_id BIGINT;
+    existing_client_id TEXT;
+    hash_              CHAR(64);
+BEGIN
+    SELECT encode(sha256(bytes_), 'hex') INTO hash_;
+
+    SELECT o.id, ou.client_id
+    INTO existing_object_id, existing_client_id
+    FROM objects o
+             INNER JOIN object_urls ou ON ou.object_id = o.id
+    WHERE o.hash = hash_;
+
+    IF existing_object_id IS NOT NULL THEN
+        RAISE EXCEPTION 'Tried to insert existing object % ', url_ ;
+    ELSE
+        IF existing_client_id IS NOT NULL AND existing_client_id <> client_id_ THEN
+            RAISE EXCEPTION 'Tried to use object of a different client % ', url_ ;
+        ELSE
+            WITH z AS (
+                INSERT INTO objects (hash, content) VALUES (hash_, bytes_)
+                    RETURNING id
+            )
+            SELECT id
+            INTO existing_object_id
+            FROM z;
+        END IF;
+    END IF;
+
+    INSERT INTO object_urls(url, object_id, client_id)
+    VALUES (url_, existing_object_id, client_id_);
+
+    INSERT INTO object_log(version, object_id, operation, hash)
+    VALUES (version_, existing_object_id, 'INS', hash_);
+END
+$body$
+    LANGUAGE plpgsql;
+
+
+
+-- Auxiliary functions for client_id lock
+CREATE OR REPLACE FUNCTION acquire_client_id_lock(client_id_ TEXT) RETURNS VOID AS
+$$
+SELECT pg_advisory_lock(1234567, CAST(hashed % 1234567 AS INT))
+FROM (SELECT hash_bigint(client_id_) AS hashed) AS z
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION hash_bigint(TEXT) RETURNS BIGINT AS
+$$
+SELECT ('x' || substr(md5($1), 1, 16)) :: BIT(64) :: BIGINT;
+$$ LANGUAGE SQL;
+
+COMMIT;
--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -2,23 +2,54 @@ BEGIN;
 
 DROP TABLE IF EXISTS object_urls;
 DROP TABLE IF EXISTS objects;
+DROP TABLE IF EXISTS object_log;
+DROP TABLE IF EXISTS versions;
 
-CREATE TABLE objects (
-    id BIGSERIAL PRIMARY KEY,    
-    hash CHAR(64) NOT NULL,
-    content BYTEA NOT NULL    
+CREATE TABLE objects
+(
+    id      BIGSERIAL PRIMARY KEY,
+    hash    CHAR(64) NOT NULL,
+    content BYTEA    NOT NULL
 );
 
-CREATE TABLE object_urls (
-    url TEXT PRIMARY KEY,
+CREATE TABLE object_urls
+(
+    url       TEXT PRIMARY KEY,
     object_id BIGINT NOT NULL,
-    client_id TEXT NOT NULL,
+    client_id TEXT   NOT NULL,
     CONSTRAINT fk_objects_urls_to_object
-      FOREIGN KEY(object_id) 
-	  REFERENCES objects(id) ON DELETE CASCADE
+        FOREIGN KEY (object_id)
+            REFERENCES objects (id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX idx_objects_hash ON objects(hash);
-CREATE INDEX idx_object_urls_client_id ON object_urls(client_id);
+CREATE TABLE versions
+(
+    version    BIGINT PRIMARY KEY,
+    session_id TEXT NOT NULL,
+    state      TEXT NOT NULL DEFAULT 'pending',
+    serial     BIGINT,
+    CHECK (state IN ('pending', 'generated'))
+);
+
+CREATE TABLE object_log
+(
+    id        BIGSERIAL PRIMARY KEY,
+    version   BIGINT,
+    object_id BIGINT,
+    operation CHAR(3)  NOT NULL,
+    hash      CHAR(64) NOT NULL,
+    CHECK (operation IN ('INS', 'UPD', 'DEL')),
+    CONSTRAINT fk_object_log_to_version
+        FOREIGN KEY (version)
+            REFERENCES versions (version) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX idx_objects_hash ON objects (hash);
+CREATE INDEX idx_object_urls_client_id ON object_urls (client_id);
+
+-- Only one version in the pending state is allowed (it must be the last one)
+CREATE UNIQUE INDEX idx_uniq_versions_version ON versions (version);
+CREATE UNIQUE INDEX idx_uniq_versions_only_one_pending ON versions (state) WHERE state = 'pending';
+CREATE UNIQUE INDEX idx_uniq_versions_generates ON versions (session_id, serial);
 
 COMMIT;
