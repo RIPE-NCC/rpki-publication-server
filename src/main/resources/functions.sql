e80b3d67f78c81825f8ab9752632e994420aac9f
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-08-28 16:27:57 +0200

    Fix tests.

--- src/main/resources/functions.sql
+++ src/main/resources/functions.sql
@@ -14,41 +14,27 @@ BEGIN
     FROM versions
     WHERE state = 'pending';
 
-    IF FOUND THEN
-        RETURN the_version;
-    ELSE
+    IF NOT FOUND THEN
         SELECT version + 1, session_id, serial
         INTO the_version, the_session_id, the_serial
         FROM versions
         ORDER BY version DESC
         LIMIT 1;
 
-        INSERT INTO versions(version, session_id, state, serial)
-        VALUES (the_version, the_session_id, 'pending', the_serial + 1);
+        IF FOUND THEN
+            INSERT INTO versions(version, session_id, state, serial)
+            VALUES (the_version, the_session_id, 'pending', the_serial + 1);
+        ELSE
+            -- generate a random UUID (without PG extensions)
+            the_version = 1;
+            INSERT INTO versions(version, session_id, state, serial)
+            VALUES (the_version, uuid_in(md5(random()::text || clock_timestamp()::TEXT)::CSTRING), 'pending', 1);
+        END IF;
     END IF;
 
     PERFORM pg_advisory_unlock(9999);
 
     RETURN the_version;
-END
-$body$
-    LANGUAGE plpgsql;
-
-
--- TODO Generate session id internally
-CREATE OR REPLACE FUNCTION init_versions(session_id_ TEXT) RETURNS BIGINT AS
-$body$
-BEGIN
-    PERFORM pg_advisory_lock(9999);
-
-    IF (SELECT COUNT(*) FROM versions) = 0 THEN
-        INSERT INTO versions(version, session_id, state, serial)
-        VALUES (1, session_id_, 'pending', 1);
-    END IF;
-
-    PERFORM pg_advisory_unlock(9999);
-
-    RETURN 1;
 END
 $body$
     LANGUAGE plpgsql;
@@ -84,31 +70,21 @@ CREATE OR REPLACE FUNCTION create_object(version_ BIGINT,
 $body$
 DECLARE
     existing_object_id BIGINT;
-    existing_client_id TEXT;
     hash_              CHAR(64);
 BEGIN
-    SELECT encode(sha256(bytes_), 'hex') INTO hash_;
+    SELECT LOWER(encode(sha256(bytes_), 'hex')) INTO hash_;
 
-    SELECT o.id, ou.client_id
-    INTO existing_object_id, existing_client_id
-    FROM objects o
-             INNER JOIN object_urls ou ON ou.object_id = o.id
-    WHERE o.hash = hash_;
+    SELECT id INTO existing_object_id
+    FROM objects WHERE hash = hash_;
 
-    IF existing_object_id IS NOT NULL THEN
-        RAISE EXCEPTION 'Tried to insert existing object % ', url_ ;
-    ELSE
-        IF existing_client_id IS NOT NULL AND existing_client_id <> client_id_ THEN
-            RAISE EXCEPTION 'Tried to use object of a different client % ', url_ ;
-        ELSE
-            WITH z AS (
-                INSERT INTO objects (hash, content) VALUES (hash_, bytes_)
-                    RETURNING id
-            )
-            SELECT id
-            INTO existing_object_id
-            FROM z;
-        END IF;
+    IF existing_object_id IS NULL THEN
+        WITH z AS (
+            INSERT INTO objects (hash, content)
+            VALUES (hash_, bytes_)
+            RETURNING id
+        )
+        SELECT id INTO existing_object_id
+        FROM z;
     END IF;
 
     INSERT INTO object_urls(url, object_id, client_id)
@@ -137,19 +113,19 @@ DECLARE
     existing_client_id TEXT;
     hash_              CHAR(64);
 BEGIN
-    SELECT encode(sha256(bytes_), 'hex') INTO hash_;
+    SELECT LOWER(encode(sha256(bytes_), 'hex')) INTO hash_;
 
     SELECT o.id, ou.client_id
     INTO existing_object_id, existing_client_id
     FROM objects o
     INNER JOIN object_urls ou ON ou.object_id = o.id
-    WHERE o.hash = hash_to_replace;
+    WHERE o.hash = LOWER(hash_to_replace);
 
     IF NOT FOUND THEN
         RAISE EXCEPTION 'Tried to replace object that doesn''t exist % ', url_ ;
     ELSE
         -- NOTE: reference in object_urls will be removed by the cascading constraint
-        DELETE FROM objects WHERE hash = hash_to_replace;
+        DELETE FROM objects WHERE hash = LOWER(hash_to_replace);
     END IF;
 
     IF existing_client_id IS NOT NULL AND existing_client_id <> client_id_ THEN
@@ -191,13 +167,13 @@ BEGIN
     INTO existing_object_id, existing_client_id, existing_url
     FROM objects o
     INNER JOIN object_urls ou ON ou.object_id = o.id
-    WHERE o.hash = hash_to_delete;
+    WHERE o.hash = LOWER(hash_to_delete);
 
     IF existing_client_id IS NOT NULL AND existing_client_id <> client_id_ THEN
         RAISE EXCEPTION 'Tried to delete object of a different client % ', existing_url ;
     ELSE
         -- NOTE: reference in object_urls will be removed by the cascading constraint
-        DELETE FROM objects WHERE hash = hash_to_delete;
+        DELETE FROM objects WHERE hash = LOWER(hash_to_delete);
     END IF;
 
     INSERT INTO object_log(version, operation, old_hash)
@@ -214,6 +190,7 @@ SELECT pg_advisory_lock(1234567, CAST(hashed % 1234567 AS INT))
 FROM (SELECT hash_bigint(client_id_) AS hashed) AS z
 $$ LANGUAGE SQL;
 
+--
 CREATE OR REPLACE FUNCTION hash_bigint(TEXT) RETURNS BIGINT AS
 $$
 SELECT ('x' || substr(md5($1), 1, 16)) :: BIT(64) :: BIGINT;
