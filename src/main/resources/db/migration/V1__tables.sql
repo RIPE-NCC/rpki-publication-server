ddc00136187e7695f99194ad3353e52bfcf43b8f
Erik Rozendaal <erik@deler.org>
2021-06-22 09:00:23 +0200

    Ensure all published objects are part of a delta when freezing version
    Previously it was possible that an in-progress transaction would be
    missing from a delta due to the non-transactional nature of sequence
    number generation.
    
    The main problem is that `object_log.id` may not be strictly
    sequential, but we depend on this when querying a specific
    version (since we only store the highest `object_log.id` per version).
    
    Scenario:
    
    - Client 1 starts publishing a million objects (getting ids 1
      ... 499_999, 500_001 ... 1_000_001)
    
    - Client 2 publishes one object (getting id 500_000) and commits.
    
    - Freeze version is called, creating a version with highest id
      500_000. The delta only contains the object from client 2, since
      client 1 has not committed yet.
    
    - Client 1 commits, making objects 1 ... 499_999, 500_001
      ... 1_000_001 visible.
    
    - Freeze version is called, creating a version with highest id
      1_000_001. The delta contains objects 500_001 to 1_000_001, missing
      the first 499_999 objects.
    
    To solve this we now explicitly link `object_log` entries to the
    version record ensuring even entries with lower `id` than
    `last_log_entry_id` are part of the new delta.

--- src/main/resources/db/migration/V1__tables.sql
+++ src/main/resources/db/migration/V1__tables.sql
@@ -17,33 +17,12 @@ CREATE UNIQUE INDEX idx_objects_url ON objects (url) WHERE deleted_at IS NULL;
 CREATE UNIQUE INDEX idx_objects_hash ON objects (hash);
 CREATE INDEX idx_objects_client_id ON objects (client_id);
 
-CREATE TABLE object_log
-(
-    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    operation     TEXT   NOT NULL,
-    new_object_id BIGINT,
-    old_object_id BIGINT,
-    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    CHECK (CASE operation
-           WHEN 'INS' THEN old_object_id IS NULL AND new_object_id IS NOT NULL
-           WHEN 'UPD' THEN new_object_id IS NOT NULL AND new_object_id IS NOT NULL
-           WHEN 'DEL' THEN old_object_id IS NOT NULL AND new_object_id IS NULL
-           ELSE FALSE
-           END
-        ),
-    FOREIGN KEY (new_object_id) REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    FOREIGN KEY (old_object_id) REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
-
-CREATE INDEX idx_object_log_new_object_id ON object_log (new_object_id) WHERE new_object_id IS NOT NULL;
-CREATE INDEX idx_object_log_old_object_id ON object_log (old_object_id) WHERE old_object_id IS NOT NULL;
 
 CREATE TABLE versions
 (
     id                 BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     session_id         TEXT                     NOT NULL,
     serial             BIGINT                   NOT NULL,
-    last_log_entry_id  BIGINT                   NOT NULL,
     snapshot_file_name TEXT,
     delta_file_name    TEXT,
     snapshot_hash      TEXT CHECK (snapshot_hash ~ '^[0-9a-f]{64}$'),
@@ -72,5 +51,29 @@ CREATE TABLE versions
         )
 );
 
-
 CREATE UNIQUE INDEX idx_versions_session_id_serial ON versions (session_id, serial);
+
+
+CREATE TABLE object_log
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    operation     TEXT   NOT NULL,
+    new_object_id BIGINT,
+    old_object_id BIGINT,
+    version_id    BIGINT,
+    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    CHECK (CASE operation
+           WHEN 'INS' THEN old_object_id IS NULL AND new_object_id IS NOT NULL
+           WHEN 'UPD' THEN new_object_id IS NOT NULL AND new_object_id IS NOT NULL
+           WHEN 'DEL' THEN old_object_id IS NOT NULL AND new_object_id IS NULL
+           ELSE FALSE
+           END
+        ),
+    FOREIGN KEY (new_object_id) REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY (old_object_id) REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY (version_id) REFERENCES versions (id) ON DELETE CASCADE ON UPDATE RESTRICT
+);
+
+CREATE INDEX idx_object_log_new_object_id ON object_log (new_object_id) WHERE new_object_id IS NOT NULL;
+CREATE INDEX idx_object_log_old_object_id ON object_log (old_object_id) WHERE old_object_id IS NOT NULL;
+CREATE INDEX idx_object_log_version_id ON object_log (version_id);
