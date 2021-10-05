9530e7b3859efc1f9bebb2d87da772d0bf111409
Erik Rozendaal <erik@deler.org>
2021-10-05 15:30:55 +0200

    Fix log test setup

--- src/test/scala/net/ripe/rpki/publicationserver/TestLogSetup.scala
+++ src/test/scala/net/ripe/rpki/publicationserver/TestLogSetup.scala
@@ -1,5 +1,5 @@
 package net.ripe.rpki.publicationserver
 
 trait TestLogSetup {
-  System.setProperty("LOG_FILE", "publication-server-test.log")
+  System.setProperty("log.file", "publication-server-test.log")
 }
